fb2ef7b9c12446b26c2ac763400e442708130d2a
Matthew <smearygeary613@gmail.com>
2025-08-09 12:17:01 -0600

    fixes to project

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import { initialTodos, validationConfig } from "../utils/constants.js";
 import Todo from "../components/Todo.js";
 import FormValidator from "../components/FormValidator.js";
 import Section from "../components/Section.js";
-import PopupwithForm from "../components/PopupwithForm.js";
+import PopupwithForm from "../components/PopupWithForm.js";
 import TodoCounter from "../components/TodoCounter.js";
 
 const addTodoButton = document.querySelector(".button_action_add");
